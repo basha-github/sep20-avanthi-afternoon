73b70c1065277f15c328fa6fb585622400d1c810
basha-github <sharifabee.alur@gmail.com>
2024-09-20 14:37:23 +0530

    first commit

---
+++ PattrenOne.java
@@ -0,0 +1,34 @@
+
+public class PattrenOne {
+
+	public static void main(String []abc) {
+	/*
+	 * 1
+	 * 2 2
+	 * 3 3 3
+	 * 4 4 4 4
+	 * 5 5 5 5 5
+	 */
+		
+		for(int r=1;r<=5;r++) {
+			for(int c=1;c<=r;c++) {
+			System.out.print(r+" ");	
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
